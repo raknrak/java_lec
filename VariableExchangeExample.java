f7294f3de23ff197ac3df658760f72b26375a784
Gyeongrak <peter6617@gmail.com>
2023-09-26 17:24:15 +0900

    조건문 실습

--- VariableExchangeExample.java
+++
@@ -1,9 +0,0 @@
-package com.mystudy.run;
-
-public class VariableExchangeExample {
-    public static void main(String[] args) {
-        String sum1 = "3" + 7;
-        //int sum2 = "3" + 7;
-        System.out.println(sum1);
-    }
-}
