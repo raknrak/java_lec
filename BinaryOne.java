3a19672bb6a5080419279c6393fe1c6a9bb2b397
Gyeongrak <peter6617@gmail.com>
2023-10-04 17:13:58 +0900

    2진수의논리와 출력 방식

---
+++ BinaryOne.java
@@ -0,0 +1,17 @@
+package com.mystudy.binary;
+
+public class BinaryOne {
+    public static void main(String[] args) {
+        int i=7;
+        int j=5;
+       // System.out.println(i>5 && j<7); // true
+       //System.out.println(i>5 && j>7); // false
+       System.out.println(i|j); //7
+        //System.out.println(i & j);
+        //2진수로 계산 => 7 = 111, 5 = 101 -> 5 출력
+        //2진수논리에는 참과 거짓이 아닌 숫자가 존재
+
+
+    }
+
+}
