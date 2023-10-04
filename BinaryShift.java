e275a355ba38d55fbc3f9c02b6f972025d1a5bc4
Gyeongrak <peter6617@gmail.com>
2023-10-04 17:15:13 +0900

    2진수의 논리연산자에는 1과 0만 존재하는 것이 아닌 숫자도 존재

---
+++ BinaryShift.java
@@ -0,0 +1,13 @@
+package com.mystudy.binary;
+
+public class BinaryShift {
+    public static void main(String[] args) {
+        int i = 13;
+        System.out.println("i<<1 : " + (i << 1));
+        System.out.println("i<<2 : " + (i << 2));
+        System.out.println("i>>1 : " + (i >> 1));
+        System.out.println("i>>2 : " + (i >> 2));
+        //2진수의 논리 연산자에는 1과 0이 존재하는 것이 아니라 숫자가 존재한다
+        //참과 거짓이 아니라 숫자가 비교 함
+    }
+}
