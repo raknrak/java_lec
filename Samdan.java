eb75ec5f061a56d2ead1911c2b9d07ddbc337548
Gyeongrak <peter6617@gmail.com>
2023-10-05 09:06:54 +0900

    숫자를 변경하여 원하는 구구단 출력 및 변수 반복

---
+++ Samdan.java
@@ -0,0 +1,15 @@
+package xece;
+
+public class Samdan {
+    public static void main(String[] args) {
+        int dan = 3; // 원하는 숫자를 입력하면 구구단 바뀜
+        for (int i = 1; i<= 9; i++) { // i가 1이 되면 계속 만족이라 반복
+             System.out.printf("%2d * %2d = %2d", dan, i, dan*i);
+            System.out.println(); //다음줄로 넘기기
+           // System.out.printf("%2d / %2d = %4.2f", dan, i, (float) dan / i);
+//            System.out.println();
+
+        }
+    }
+}
+
