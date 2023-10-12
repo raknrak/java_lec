334568f6a212eb6ee506636129933c76450c7fa2
Gyeongrak <peter6617@gmail.com>
2023-10-12 16:38:05 +0900

    다중 상속 및 추상 메소드 오버라이딩 및 호출

---
+++ RemoteControl.java
@@ -0,0 +1,8 @@
+package com.mystudy.interfac.exam03;
+
+public interface RemoteControl {
+    // 추상 메소드
+    void turnOn();
+    void turnOff();
+
+}
---
+++ Searchable.java
@@ -0,0 +1,7 @@
+package com.mystudy.interfac.exam03;
+
+public interface Searchable {
+    //추상 메소드
+    void search(String url);
+
+}
