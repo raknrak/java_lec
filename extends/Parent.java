fd9027e070be4e6fee19fcafc40736b9c8cfadc0
Gyeongrak <peter6617@gmail.com>
2023-10-20 19:30:01 +0900

    delete folder

--- extends/Parent.java
+++
@@ -1,9 +0,0 @@
-package com.mystudy.work02.extendds;
-
-public class Parent {
-    //정수형 변수 x 선언 및 100으로 초기화
-    int x= 100;
-    void method() {
-        System.out.println("Parent Method");
-    }
-}
